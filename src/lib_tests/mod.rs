79aa0f4460d082c9ac9b77c95b3c8d559c7bc9e6
Chen Xiao <chenxiao770117@gmail.com>
2024-10-01 08:45:55 +0800

    update

---
+++ src/lib_tests/mod.rs
@@ -0,0 +1,15 @@
+use super::*;
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    fn void_svg_out_test() {
+        let svg = svg_out(2, 2);
+        
+        let mut file = File::open("../../
+        
+        assert_eq!(svg, "4");
+    }
+}
