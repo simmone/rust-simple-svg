d2203e1f8c00f45893ee6cb5a08d61b8682a6aed
Chen Xiao <chenxiao770117@gmail.com>
2024-10-12 15:45:37 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 use crate::defines::svg::Svg;
 
 pub mod defines;
+pub mod lib_tests;
 
 pub fn svg_out(svg: Svg) -> String {
     format!("<svg\n{}, {}", svg.width.to_string(), svg.height.to_string())
 }
 
-#[cfg(test)]
-mod lib_tests;
+
