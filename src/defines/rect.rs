0f468f451e8c3e69acd888d2ee486460795465f8
Chen Xiao <chenxiao770117@gmail.com>
2024-11-14 11:16:51 +0800

    update

--- src/defines/rect.rs
+++ src/defines/rect.rs
@@ -3,25 +3,25 @@ use crate::defines::shape::Shape;
 pub struct Rect {
     pub width: f64,
     pub height: f64,
-    pub radius_x: u32,
-    pub radius_y: u32,
+    pub radius_x: i32,
+    pub radius_y: i32,
 }
 
 impl Shape for Rect {
     fn format(&self, shape_id: String) -> String {
         format!("    <rect id=\"{}\" {} />\n",
         shape_id,
-        || {
+        {
             let mut shape_str = format!("width=\"{}\" height=\"{}\"",
                                         self.width, self.height);
 
-            when (self.radius_x >= 0) && (self.radius_y >= 0) {
+            if (self.radius_x >= 0) && (self.radius_y >= 0) {
                 shape_str.push_str(&format!(" rx=\"{}\" ry=\"{}\"",
                                             self.radius_x, self.radius_y));
             }
 
             shape_str
-        });
+        })
     }
 }
 
@@ -40,6 +40,6 @@ mod tests {
         assert_eq!(rect.width, 30.0);
         assert_eq!(rect.height, 20.0);
         assert_eq!(rect.radius_x, 10);
-        assert_eq!(rect.radius_y, 6);
+        assert_eq!(rect.radius_y, 5);
     }
 }
