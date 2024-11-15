26a0afe5bfff0ef47e022bc1b134ce9a44481b0d
Chen Xiao <chenxiao770117@gmail.com>
2024-11-15 17:38:12 +0800

    update

--- src/defines/rect.rs
+++ src/defines/rect.rs
@@ -3,21 +3,30 @@ use crate::defines::shape::Shape;
 pub struct Rect {
     pub width: f64,
     pub height: f64,
-    pub radius_x: i32,
-    pub radius_y: i32,
+    pub radius_x: Option<i32>,
+    pub radius_y: Option<i32>,
+}
+
+pub fn build_rect(width: f64, height: f64) -> Rect {
+    Rect {
+        width,
+        height,
+        radius_x: None,
+        radius_y: None,
+    }
 }
 
 impl Shape for Rect {
     fn format(&self, shape_id: String) -> String {
-        format!("    <rect id=\"{}\" {} />\n",
-        shape_id,
-        {
-            let mut shape_str = format!("width=\"{}\" height=\"{}\"",
-                                        self.width, self.height);
+        format!("    <rect id=\"{}\" {} />\n", shape_id, {
+            let mut shape_str = format!("width=\"{}\" height=\"{}\"", self.width, self.height);
 
-            if (self.radius_x >= 0) && (self.radius_y >= 0) {
-                shape_str.push_str(&format!(" rx=\"{}\" ry=\"{}\"",
-                                            self.radius_x, self.radius_y));
+            if self.radius_x.is_some() && self.radius_y.is_some() {
+                shape_str.push_str(&format!(
+                    " rx=\"{}\" ry=\"{}\"",
+                    self.radius_x.unwrap(),
+                    self.radius_y.unwrap()
+                ));
             }
 
             shape_str
@@ -34,12 +43,12 @@ mod tests {
         let rect = Rect {
             width: 30.0,
             height: 20.0,
-            radius_x: 10,
-            radius_y: 5,
+            radius_x: Some(10),
+            radius_y: Some(5),
         };
         assert_eq!(rect.width, 30.0);
         assert_eq!(rect.height, 20.0);
-        assert_eq!(rect.radius_x, 10);
-        assert_eq!(rect.radius_y, 5);
+        assert_eq!(rect.radius_x.unwrap(), 10);
+        assert_eq!(rect.radius_y.unwrap(), 5);
     }
 }
