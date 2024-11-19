e0ee8a4848b8e639cded3270b1959a12992454f3
Chen Xiao <chenxiao770117@gmail.com>
2024-11-19 11:02:50 +0800

    update

--- src/defines/rect.rs
+++ src/defines/rect.rs
@@ -1,4 +1,5 @@
 use crate::defines::shape::Shape;
+use std::fmt::Display;
 
 pub struct Rect<T, U, W, Y> {
     pub width: T,
@@ -7,20 +8,22 @@ pub struct Rect<T, U, W, Y> {
     pub radius_y: Option<Y>,
 }
 
-pub fn build_rect<T, U, W, Y>(width: T, height: U) -> Rect<T, U, W, Y> {
-    Rect {
-        width,
-        height,
-        radius_x: None,
-        radius_y: None,
+impl<T, U, W, Y> Rect<T, U, W, Y> {
+    pub fn new(width: T, height: U) -> Self {
+        Rect {
+            width,
+            height,
+            radius_x: None,
+            radius_y: None,
+        }
     }
 }
 
 impl<T, U, W, Y> Shape for Rect<T, U, W, Y> where
-    T: std::fmt::Display,
-    U: std::fmt::Display,
-    W: std::fmt::Display,
-    Y: std::fmt::Display,
+    T: Display,
+    U: Display,
+    W: Display,
+    Y: Display,
 {
     fn format(&self, shape_id: String) -> String {
         format!("    <rect id=\"{}\" {} />\n", shape_id, {
@@ -44,7 +47,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn check_new_rect() {
+    fn check_new_rect1() {
         let rect = Rect {
             width: 30.0,
             height: 20.0,
@@ -57,21 +60,64 @@ mod tests {
         assert_eq!(rect.radius_x.unwrap() as f64, 10.0);
         assert_eq!(rect.radius_y.unwrap() as f64, 5.0);
     }
+
+    #[test]
+    fn check_new_rect2() {
+        let rect = Rect {
+            width: 30.0,
+            height: 20.0,
+            radius_x: Some(10),
+            radius_y: Some(5.0),
+        };
+
+        assert_eq!(rect.radius_x.unwrap() as f64, 10.0);
+        assert_eq!(rect.radius_y.unwrap() as f64, 5.0);
+    }
+
+    #[test]
+    fn check_new_rect3() {
+        let rect = Rect {
+            width: 30.0,
+            height: 20.0,
+            radius_x: Some(10.0),
+            radius_y: Some(5),
+        };
+
+        assert_eq!(rect.radius_x.unwrap() as f64, 10.0);
+        assert_eq!(rect.radius_y.unwrap() as f64, 5.0);
+    }
+
+    #[test]
+    fn check_new_rect4() {
+        let rect = Rect {
+            width: 30.0,
+            height: 20.0,
+            radius_x: Some(10.0),
+            radius_y: Some(5.0),
+        };
+
+        assert_eq!(rect.radius_x.unwrap() as f64, 10.0);
+        assert_eq!(rect.radius_y.unwrap() as f64, 5.0);
+    }
     
     #[test]
-    fn check_build_rect() {
-        let rect = build_rect(30, 20);
-
+    fn check_new() {
+        let rect = Rect::new(30, 20);
         assert_eq!(rect.width as f64, 30.0);
         assert_eq!(rect.height as f64, 20.0);
         assert!(rect.radius_x.is_none());
         assert!(rect.radius_y.is_none());
 
-        let rect = build_rect(30.0, 20);
-
+        let rect = Rect::new(30.0, 20);
         assert_eq!(rect.width as f64, 30.0);
         assert_eq!(rect.height as f64, 20.0);
-        assert!(rect.radius_x.is_none());
-        assert!(rect.radius_y.is_none());
+
+        let rect = Rect::new(30, 20.0);
+        assert_eq!(rect.width as f64, 30.0);
+        assert_eq!(rect.height as f64, 20.0);
+
+        let rect = Rect::new(30.0, 20.0);
+        assert_eq!(rect.width as f64, 30.0);
+        assert_eq!(rect.height as f64, 20.0);
     }
 }
