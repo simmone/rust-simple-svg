099d2495b4ee27719f7cc4afb0efbcdbc12e431a
Chen Xiao <chenxiao770117@gmail.com>
2024-12-10 10:45:01 +0800

    update

--- tests/empty_svg_test.rs
+++ tests/empty_svg_test.rs
@@ -4,7 +4,7 @@ use simple_svg::*;
 #[test]
 fn empty_svg_out_test() {
     let svg = build_svg(30.0, 20.0);
-
+    
     let svg_str = svg_out(svg);
 
     let contents = include_str!("../showcase/basic/empty.svg");
