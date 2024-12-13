3c8e27b21400c7b82f70c323acf9a3f73abf2550
Chen Xiao <chenxiao770117@gmail.com>
2024-12-13 22:44:43 +0800

    update

--- src/defines.rs
+++ src/defines.rs
@@ -3,4 +3,5 @@ pub mod rect;
 pub mod shape;
 pub mod sstyle;
 pub mod widget;
+pub mod group;
 pub mod svg;
--- src/defines/group.rs
+++ src/defines/group.rs
@@ -3,3 +3,23 @@ use crate::defines::widget::Widget;
 pub struct Group {
     pub widget_list: Vec<Widget>,
 }
+
+impl Group {
+    pub fn new() -> Self {
+        Group {
+            widget_list: Vec::new(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn check_new() {
+        let group = Group::new();
+        
+        assert_eq!(group.widget_list.len(), 0);
+    }
+}
