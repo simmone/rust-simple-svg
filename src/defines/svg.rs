bc9558ef04dbbbaa6cdb837b60df809c21f0bc11
Chen Xiao <chenxiao770117@gmail.com>
2024-12-14 23:09:16 +0800

    update

--- src/defines/svg.rs
+++ src/defines/svg.rs
@@ -18,7 +18,7 @@ impl Svg {
         }
     }
 
-    pub fn def_shape(&mut self, shape: Shape) -> String {
+    pub fn add_shape(&mut self, shape: Shape) -> String {
         self.widget_id_count += 1;
         let shape_id = format!("s{}", self.widget_id_count);
         self.shape_define_map.insert(shape_id.clone(), shape);
@@ -42,12 +42,12 @@ mod tests {
     }
 
     #[test]
-    fn check_def_shape() {
+    fn check_add_shape() {
         let mut svg: Svg = Svg::new(640.0, 480.0);
 
         let rect1 = Rect::new(30.0, 20.0);
         let shape1 = Shape::Rect(rect1);
-        let _rect_id = svg.def_shape(shape1);
+        let _rect_id = svg.add_shape(shape1);
         assert_eq!(svg.widget_id_count, 1);
         match svg.shape_define_map.get("s1").unwrap() {
             Shape::Rect(s1) => {
@@ -57,7 +57,7 @@ mod tests {
 
         let rect2 = Rect::new(10.0, 5.0);
         let shape2 = Shape::Rect(rect2);
-        let _rect_id = svg.def_shape(shape2);
+        let _rect_id = svg.add_shape(shape2);
         assert_eq!(svg.widget_id_count, 2);
         match svg.shape_define_map.get("s2").unwrap() {
             Shape::Rect(s2) => {
--- tests/rect_test.rs
+++ tests/rect_test.rs
@@ -2,12 +2,28 @@ use pretty_assertions::assert_eq;
 use simple_svg::*;
 
 #[test]
-fn empty_svg_out_test() {
+fn rect_svg_out_test() {
     let svg = Svg::new(30.0, 20.0);
+    
+    let rect = Shape::Rect(Rect::new(100.0, 100.0));
+
+    let rect_id = svg.add_shape(rect);
+    
+    let rect_sstyle = Sstyle::new();
+    rect_sstyle.fill = Some("#BBC42A".to_string());
+
+    svg.place_widget(rect_id, 
 
     let svg_str = svg_out(svg);
 
-    let contents = include_str!("../showcase/basic/empty.svg");
+    let contents = include_str!("../showcase/shapes/rect/rect.svg");
 
     assert_eq!(svg_str, contents);
 }
+
+            (lambda ()
+              (let ([rec_id (svg-def-shape (new-rect 100 100))]
+                    [_sstyle (sstyle-new)])
+                (set-SSTYLE-fill! _sstyle "#BBC42A")
+                (svg-place-widget rec_id #:style _sstyle))))])
+
