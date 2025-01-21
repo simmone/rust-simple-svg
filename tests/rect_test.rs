ac8105c86ffa5a3ce66275039091af9613fa405b
Chen Xiao <chenxiao770117@gmail.com>
2025-01-21 17:56:33 +0800

    update

--- tests/rect_test.rs
+++ tests/rect_test.rs
@@ -25,3 +25,29 @@ fn rect_test() {
 
     assert_eq!(svg_str, contents);
 }
+
+#[test]
+fn rect_y_test() {
+    let mut svg = Svg::new(100.0, 100.0);
+
+    let rect_id = svg.add_shape(Shape::Rect(Rect::new(100.0, 100.0)));
+
+    let mut rect_sstyle = Sstyle::new();
+    rect_sstyle.fill = Some("#BBC42A".to_string());
+
+    let mut group = Group::new();
+    group.place_widget(Widget {
+        shape_id: rect_id,
+        style: Some(rect_sstyle),
+        at: Some((50.0, 50.0)),
+        ..Default::default()
+    });
+
+    svg.add_default_group(group);
+
+    let svg_str = svg_out(svg);
+
+    let contents = include_str!("../showcase/shapes/rect/rect_y.svg");
+
+    assert_eq!(svg_str, contents);
+}
