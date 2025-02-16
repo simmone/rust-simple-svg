3de728d131e62dba75af3dd9a1377d43a2612a75
Chen Xiao <chenxiao770117@gmail.com>
2025-02-17 06:09:34 +0800

    update

--- src/defines/path.rs
+++ src/defines/path.rs
@@ -1,10 +1,12 @@
+use std::fmt;
+
 #[derive(Debug, Clone)]
 pub struct Path {
     pub defs: Vec<String>,
 }
 
 #[derive(Clone)]
-ArcDirection {
+pub enum ArcDirection {
     LeftBig,
     LeftSmall,
     RightBig,
@@ -14,19 +16,17 @@ ArcDirection {
 impl fmt::Display for ArcDirection {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            FillRule::LeftBig => write!(f, "1, 0"),
-            FillRule::LeftSmall => write!(f, "0, 0"),
-            FillRule::RightBig => write!(f, "1, 1"),
-            FillRule::RightSmall => write!(f, "0, 1"),
+            ArcDirection::LeftBig => write!(f, "1, 0"),
+            ArcDirection::LeftSmall => write!(f, "0, 0"),
+            ArcDirection::RightBig => write!(f, "1, 1"),
+            ArcDirection::RightSmall => write!(f, "0, 1"),
         }
     }
 }
 
 impl Path {
     pub fn new() -> Self {
-        Path {
-            defs: vec![],
-        }
+        Path { defs: vec![] }
     }
 
     pub fn lineto_abs(&mut self, point: (f64, f64)) {
@@ -46,11 +46,61 @@ impl Path {
     }
 
     pub fn arc_abs(&mut self, point: (f64, f64), radius: (f64, f64), section: ArcDirection) {
-        self.defs.push(format!("A{},{} 0 {} {},{}", radius.0, radius.1, section, point.0, point.1));
+        self.defs.push(format!(
+            "A{},{} 0 {} {},{}",
+            radius.0, radius.1, section, point.0, point.1
+        ));
     }
 
     pub fn arc_rel(&mut self, point: (f64, f64), radius: (f64, f64), section: ArcDirection) {
-        self.defs.push(format!("a{},{} 0 {} {},{}", radius.0, radius.1, section, point.0, point.1));
+        self.defs.push(format!(
+            "a{},{} 0 {} {},{}",
+            radius.0, radius.1, section, point.0, point.1
+        ));
+    }
+
+    pub fn ccurve_abs(&mut self, point1: (f64, f64), point2: (f64, f64), point3: (f64, f64)) {
+        self.defs.push(format!(
+            "C{},{} {},{} {},{}",
+            point1.0, point1.1, point2.0, point2.1, point3.0, point3.1
+        ));
+    }
+
+    pub fn ccurve_rel(&mut self, point1: (f64, f64), point2: (f64, f64), point3: (f64, f64)) {
+        self.defs.push(format!(
+            "c{},{} {},{} {},{}",
+            point1.0, point1.1, point2.0, point2.1, point3.0, point3.1
+        ));
+    }
+
+    pub fn qcurve_abs(&mut self, point1: (f64, f64), point2: (f64, f64)) {
+        self.defs.push(format!(
+            "Q{},{} {},{}",
+            point1.0, point1.1, point2.0, point2.1
+        ));
+    }
+
+    pub fn qcurve_rel(&mut self, point1: (f64, f64), point2: (f64, f64)) {
+        self.defs.push(format!(
+            "q{},{} {},{}",
+            point1.0, point1.1, point2.0, point2.1
+        ));
+    }
+
+    pub fn moveto_abs(&mut self, point: (f64, f64)) {
+        self.defs.push(format!("M{},{}", point.0, point.1));
+    }
+
+    pub fn moveto_rel(&mut self, point: (f64, f64)) {
+        self.defs.push(format!("m{},{}", point.0, point.1));
+    }
+
+    pub fn raw(&mut self, raw_data: String) {
+        self.defs.push(raw_data);
+    }
+
+    pub fn close(&mut self) {
+        self.defs.push("z".to_string());
     }
 
     pub fn format(&self, shape_id: String) -> String {
@@ -79,12 +129,12 @@ mod tests {
         let path = Path::new();
 
         assert_eq!(path.defs.len(), 0);
-     }
+    }
 
     #[test]
     fn check_format() {
         let mut path = Path::new();
-        
+
         path.defs.push("path strs".to_string());
 
         assert_eq!(Path::format(&path, "1".to_string()), {
@@ -94,7 +144,7 @@ mod tests {
             c_str.push_str("          d=\"\n");
             c_str.push_str("             path strs\n");
             c_str.push_str("            \"/>\n");
-                   
+
             c_str
         });
     }
