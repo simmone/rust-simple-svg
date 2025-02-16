e4a05779551f85ca4d3cf74132f75f9cc1232e5b
Chen Xiao <chenxiao770117@gmail.com>
2025-02-16 22:25:48 +0800

    update

--- src/defines/path.rs
+++ src/defines/path.rs
@@ -3,6 +3,25 @@ pub struct Path {
     pub defs: Vec<String>,
 }
 
+#[derive(Clone)]
+ArcDirection {
+    LeftBig,
+    LeftSmall,
+    RightBig,
+    RightSmall,
+}
+
+impl fmt::Display for ArcDirection {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            FillRule::LeftBig => write!(f, "1, 0"),
+            FillRule::LeftSmall => write!(f, "0, 0"),
+            FillRule::RightBig => write!(f, "1, 1"),
+            FillRule::RightSmall => write!(f, "0, 1"),
+        }
+    }
+}
+
 impl Path {
     pub fn new() -> Self {
         Path {
@@ -11,11 +30,11 @@ impl Path {
     }
 
     pub fn lineto_abs(&mut self, point: (f64, f64)) {
-        self.defs.push(format!("l{},{}", point.0, point.1));
+        self.defs.push(format!("L{},{}", point.0, point.1));
     }
 
     pub fn lineto_rel(&mut self, point: (f64, f64)) {
-        self.defs.push(format!("L{},{}", point.0, point.1));
+        self.defs.push(format!("l{},{}", point.0, point.1));
     }
 
     pub fn lineto_hor(&mut self, length: f64) {
@@ -24,6 +43,14 @@ impl Path {
 
     pub fn lineto_ver(&mut self, length: f64) {
         self.defs.push(format!("v{}", length));
+    }
+
+    pub fn arc_abs(&mut self, point: (f64, f64), radius: (f64, f64), section: ArcDirection) {
+        self.defs.push(format!("A{},{} 0 {} {},{}", radius.0, radius.1, section, point.0, point.1));
+    }
+
+    pub fn arc_rel(&mut self, point: (f64, f64), radius: (f64, f64), section: ArcDirection) {
+        self.defs.push(format!("a{},{} 0 {} {},{}", radius.0, radius.1, section, point.0, point.1));
     }
 
     pub fn format(&self, shape_id: String) -> String {
