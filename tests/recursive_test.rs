cf71a471aaa1dfec596d33c7d4273277a990d47a
Chen Xiao <chenxiao770117@gmail.com>
2025-03-07 17:16:50 +0800

    update

--- tests/recursive_test.rs
+++ tests/recursive_test.rs
@@ -10,31 +10,43 @@ fn recursive_test() {
 
     let mut group = Group::new();
     
-    let recursive_closure = |x: f64, y: f64, radius: f64| {
-        let circle_id = svg.add_shape(Shape::Circle(Circle::new(radius)));
-
-        group.place_widget(Widget {
-            shape_id: circle_id,
-            style: Some(sstyle.clone()),
-            at: Some((x, y)),
-            ..Default::default()
-        });
+    let mut paint_branch = 
+        |mut x: f64, mut y: f64, mut radius: f64, x_func: &dyn Fn(f64, f64) -> f64, y_func: &dyn Fn(f64, f64) -> f64| {
+            loop {
+                if radius <= 8.0 { break; }
         
-        if radius > 8.0 {
-            recursive_closure(x + radius, y, radius / 2.0);
-            recursive_closure(x - radius, y, radius / 2.0);
-            recursive_closure(x, y + radius, radius / 2.0);
-            recursive_closure(x, y - radius, radius / 2.0);
-         }
-    };
-    
-    recursive_closure(200.0, 200.0, 100.0);
+                let circle_id = svg.add_shape(Shape::Circle(Circle::new(radius)));
+
+                group.place_widget(Widget {
+                    shape_id: circle_id,
+                    style: Some(sstyle.clone()),
+                    at: Some((x, y)),
+                    ..Default::default()
+                });
+                
+                x = x_func(x, radius);
+                
+                y = y_func(y, radius);
+                
+                radius = radius / 2.0;
+            }
+        };
+
+    let x_func = |x, radius| { x + radius };
+
+    let y_func = |y, _| { y };
+
+    paint_branch(200.0, 200.0, 100.0, &x_func, &y_func);
 
     svg.add_default_group(group);
 
     let svg_str = svg_out(svg);
+    
+    print!("{}\n", svg_str);
+    
+    assert_eq!(1, 2);
 
-    let contents = include_str!("../showcase/example/recursive.svg");
+//    let contents = include_str!("../showcase/example/recursive.svg");
 
-    assert_eq!(svg_str, contents);
+//    assert_eq!(svg_str, contents);
 }
