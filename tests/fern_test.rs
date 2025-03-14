0ad380eb8441ebaafaad09b306e4665b3db459c8
Chen Xiao <chenxiao770117@gmail.com>
2025-03-14 11:21:30 +0800

    update

--- tests/fern_test.rs
+++ tests/fern_test.rs
@@ -5,6 +5,21 @@ use num::complex::Complex;
 use std::f64::consts::PI;
 use simple_svg::*;
 
+const CANVAS_WIDTH: f64 = 600.0;
+const CANVAS_HEIGHT: f64 = 600.0;
+const START_POINT: (f64, f64) = (300.0, 50.0);
+const START_LENGTH: f64 = 120.0;
+const START_DEG: f64 = 100.0; // 100°
+const START_WIDTH: f64 = 3.0;
+const STEP_WIDTH: f64 = 0.86;
+const COLOR: &str = "#5A5";
+const MIN_LENGTH: f64 = 0.5;
+const CENTRAL_REDUCTION: f64 = 0.75;
+const LATERAL_REDUCTION: f64 = 0.35;
+const LATERAL_DEG: f64 = 80.0; // 80°
+const BEND: f64 = 5.0; // 5°
+const PRECISION: f64 = 0.0;
+
 fn make_polar(magnitude: f64, angle: f64) -> Complex<f64> {
     let complex_number = Complex::new(0.0, 1.0);
     
@@ -27,25 +42,16 @@ fn get_end_point(start_point: (f64, f64), length: f64, deg: f64, precision: f64)
     )
 }
 
-fn 
+fn recursive_points(loop_start_point: (f64, f64), length: f64, deg: f64, width: f64, style_map: HashMap<f64, Vec<((f64, f64), (f64, f64))>>) {
+    if (CENTRAL_REDUCTION * length) >= MIN_LENGTH {
+        let loop_end_point = get_end_point(loop_start_point, length, deg, PRECISION);
+        
+        
+    }
+}
 
 #[test]
 fn fern_test() {
-    let canvas_width = 600.0;
-    let canvas_height = 600.0;
-    let start_point = (300.0, 50.0);
-    let start_length = 120.0;
-    let start_deg = 100.0; // 100°
-    let start_width = 3.0;
-    let step_width = 0.86;
-    let color = "#5a5";
-    let min_length = 0.5;
-    let central_reduction = 0.75;
-    let lateral_reduction = 0.35;
-    let lateral_deg = 80.0; // 80°
-    let bend = 5.0; // 5°
-    let precision = 0.0;
-
     let mut svg = Svg::new(canvas_width, canvas_height);
     
     svg.add_default_group(group);
