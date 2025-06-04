cbd180c789a0016fa44b45fe65cfffb99dde24db
Chen Xiao <chenxiao770117@gmail.com>
2025-06-04 11:04:27 +0800

    update

--- src/define/shape/rect.rs
+++ src/define/shape/rect.rs
@@ -8,6 +8,7 @@ pub struct Rect {
     pub height: f64,
     pub radius_x: Option<f64>,
     pub radius_y: Option<f64>,
+    pub precision: usize,
 }
 
 impl Rect {
@@ -17,18 +18,19 @@ impl Rect {
             height,
             radius_x: None,
             radius_y: None,
+            precision: 0,
         }
     }
 
     pub fn format(&self, shape_id: String) -> String {
         format!("    <rect id=\"{}\" {} />\n", shape_id, {
-            let mut shape_str = format!("width=\"{}\" height=\"{}\"", svg_round(self.width), svg_round(self.height));
+            let mut shape_str = format!("width=\"{}\" height=\"{}\"", svg_round(self.width, self.precision), svg_round(self.height, self.precision));
 
             if self.radius_x.is_some() && self.radius_y.is_some() {
                 shape_str.push_str(&format!(
                     " rx=\"{}\" ry=\"{}\"",
-                    svg_round(self.radius_x.unwrap()),
-                    svg_round(self.radius_y.unwrap()),
+                    svg_round(self.radius_x.unwrap(), self.precision),
+                    svg_round(self.radius_y.unwrap(), self.precision),
                 ));
             }
 
@@ -55,12 +57,14 @@ mod tests {
             height: 20f64,
             radius_x: Some(10.0),
             radius_y: Some(5f64),
+            precision: 4,
         };
 
         assert_eq!(rect.width, 30.0);
         assert_eq!(rect.height, 20.0);
         assert_eq!(rect.radius_x.unwrap(), 10.0);
         assert_eq!(rect.radius_y.unwrap(), 5.0);
+        assert_eq!(rect.precision, 4);
     }
 
     #[test]
@@ -70,6 +74,7 @@ mod tests {
         assert_eq!(rect.height, 20.0);
         assert!(rect.radius_x.is_none());
         assert!(rect.radius_y.is_none());
+        assert_eq!(rect.precision, 0);
     }
 
     #[test]
@@ -89,6 +94,7 @@ mod tests {
             height: 20.00001,
             radius_x: Some(10.00001),
             radius_y: Some(5.00001),
+            precision: 4,
         };
 
         assert_eq!(
